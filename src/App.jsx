fc954e64daf0a1f369ba571e9536c0c6f8b02f01
Adela Veres <adela.veres@yahoo.ro>
2025-10-14 22:55:15 +0300

    Removed 'useSwipeable' logic, to scroll naturally on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,9 @@ import Footer from './components/Footer'
 
 
 export default function App(){
-    // handlers scroll by viewport height (works because each section is min-h-screen)
-    const handlers = useSwipeable({
-        onSwipedUp: () => 
-            window.scrollBy({ top: window.innerHeight, behavior: 'smooth' }),
-        onSwipedDown: () => 
-            window.scrollBy({ top: -window.innerHeight, behavior: 'smooth' }),
-        trackTouch: true,
-    })
-
 
 return (
-    <div {...handlers} className="font-sans">
+    <div className="font-sans">
         <Hero />
         <About />
         <Skills />
